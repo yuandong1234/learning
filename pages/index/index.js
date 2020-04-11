df1565e2deaac64446f4751d1d7e8106054b9fc6
yuandong <888@jzy.com>
2020-04-11 18:44:15 +0800

    .

---
+++ pages/index/index.js
@@ -0,0 +1,89 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    "newsList": []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.getIndexData();
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  /**
+   * 获取首页数据
+   */
+  getIndexData: function () {
+    var that = this;
+    wx.request({
+      url: 'https://v.juhe.cn/toutiao/index',
+      method: "GET",
+      data: {
+        key: 'aa47561558f285fee99f1943c7b844fb',
+        type: 'top'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log(res.data);
+        that.setData({
+          newsList: res.data.result.data
+        });
+      }
+    })
+  }
+})
